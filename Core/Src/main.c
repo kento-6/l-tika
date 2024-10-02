abf06c369ccea48b2717ad001645d05ed45b5532
kento-6 <melon.pan.131@gmail.com>
2024-10-02 17:46:49 +0900

    間隔を500から1000に変更

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,9 +100,9 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET);
-	  HAL_Delay(500);
+	  HAL_Delay(1000);
 	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET);
-	  HAL_Delay(500);
+	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
